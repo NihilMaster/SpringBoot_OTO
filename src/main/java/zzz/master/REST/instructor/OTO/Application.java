5ef2b2392212b95966afd31914e1cf5859d41c9d
mateo.nihil <mateo.nihil@gmail.com>
2024-08-13 00:36:39 -0500

    1. Initial

---
+++ src/main/java/zzz/master/REST/instructor/OTO/Application.java
@@ -0,0 +1,13 @@
+package zzz.master.REST.instructor.OTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
